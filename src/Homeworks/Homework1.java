1ccc305a2b38d22ba5280c2f101efdba56acc502
joozepp <podelsky@gmail.com>
2022-10-04 21:00:39 +0300

    advancedish stuff

---
+++ src/Homeworks/Homework1.java
@@ -0,0 +1,12 @@
+package Homeworks;
+
+/**
+ * To read a number from user and print the same number like this:
+ *Given number 4
+ * 4, 44, 444, 4444, 44444
+ */
+public class Homework1 {
+    public static void main(String[] args) {
+
+    }
+}
